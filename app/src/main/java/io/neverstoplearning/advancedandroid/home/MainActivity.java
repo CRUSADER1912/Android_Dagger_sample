1f40e19fde9d1032996789d1b0d4e92a16967ece
CRUSADER1912 <saldanha.linus@gmail.com>
2018-10-07 12:37:41 +0530

    Moved MainActivity to home package

--- app/src/main/java/io/neverstoplearning/advancedandroid/home/MainActivity.java
+++ app/src/main/java/io/neverstoplearning/advancedandroid/home/MainActivity.java
@@ -1,7 +1,9 @@
-package io.neverstoplearning.advancedandroid;
+package io.neverstoplearning.advancedandroid.home;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.neverstoplearning.advancedandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
